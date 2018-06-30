99126650139ec523c1d4079008fa4bc091643ba5
mydeskmate <516622145@qq.com>
2018-07-01 02:10:16 +0800

    new

--- django/login_work/login_work/urls.py
+++ django/login_work/login_work/urls.py
@@ -16,6 +16,51 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.shortcuts import HttpResponse,render,redirect
+import pymysql
+def login(request):
+    """
+    处理用户登录请求,返回内容
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        return render(request,'login.html')
+    else:
+        u = request.POST.get('user')
+        p = request.POST.get('pwd')
+        print(u)
+        print(p)
+        conn = pymysql.connect(host="localhost",user="root",password="123456",database="school")
+        cursor = conn.cursor()
+        sql = "select * from user where name=%s and password=%s"
+        cursor.execute(sql,[u,p])
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if result:
+            return redirect('/index/')
+        else:
+            return render(request,'login.html',{'msg':'用户名或密码错误'})
+
+def index(request):
+    conn = pymysql.connect(host="localhost",user='root',password='123456',database='school')
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    sql = "select name,password from user"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render(
+        request,
+        'index.html',
+        {
+            'user_list':result
+        }
+    )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login),
+    path('index/', index),
 ]
