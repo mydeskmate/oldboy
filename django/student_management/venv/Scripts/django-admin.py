b19e98e0cbaf9dbb94ce43596f9c3426626de1af
mydeskmate <516622145@qq.com>
2018-07-01 19:42:38 +0800

    new

---
+++ django/student_management/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\mydata\github\oldboy\django\student_management\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
