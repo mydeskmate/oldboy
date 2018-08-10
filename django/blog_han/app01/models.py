5e2165dfa8067664de1c8dd3395ed1469c7704a8
mydeskmate <516622145@qq.com>
2018-08-10 13:41:04 +0800

    new

--- django/blog_han/app01/models.py
+++ django/blog_han/app01/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32)
+    nickname = models.CharField(max_length=32)
+    email = models.EmailField(max_length=64)
+    phone = models.CharField(max_length=11)
+
+class Blog(models.Model):
+    url = models.CharField(max_length=32)
+    uf = models.OneToOneField(to="UserInfo")
+
+class Category(models.Model):
+    title = models.CharField(max_length=32)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=32)
+
+class Article(models.Model):
+    created_time = models.DateField()
+    like_count = models.IntegerField()
+    unlike_count = models.IntegerField()
+    read_count = models.IntegerField()
+    comment_count = models.IntegerField()
+    bg = models.ForeignKey(Blog)
+    cg = models.OneToOneField(Category)
+    tg = models.ManyToManyField(Tag)
+
+
+
+class CommentDetail(models.Model):
+    content = models.CharField(max_length=512)
+
+class Comment(models.Model):
+    comment_time = models.DateField()
+    cd = models.ForeignKey(CommentDetail)
+    at = models.ForeignKey(Article)
--- django/blog_han/blog_han/settings.py
+++ django/blog_han/blog_han/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app01.apps.App01Config',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'blog_han.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
